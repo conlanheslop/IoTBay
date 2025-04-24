8f0c79507c55b69b4ddda96edfb4ad60e32c101e
conlanheslop <conlanh2004@gmail.com>
2025-04-24 15:46:30 +1000

    Database initialisation

---
+++ iotbay/src/main/webapp/WEB-INF/classes/model/dao/DB.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.sql.Connection;
+
+/**
+ * Stores database information
+ */
+
+public abstract class DB {
+    protected String URL = "jdbc:sqlite:database/iotbay.db";
+    protected String driver = "org.sqlite.JDBC";
+    protected Connection conn;
+}

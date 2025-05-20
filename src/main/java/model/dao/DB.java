114b902e208583386b098bcc89611b88eb793279
kaigagi <kaigagipro@gmail.com>
2025-05-20 16:45:52 +1000

    update local database path

--- src/main/java/model/dao/DB.java
+++ src/main/java/model/dao/DB.java
@@ -7,11 +7,7 @@ import java.sql.Connection;
  */
 
 public abstract class DB {
-<<<<<<< HEAD:iotbay/src/main/java/model/dao/DB.java
-    protected String URL = "jdbc:sqlite:D:/Projects/ISD/iotbay/database/iotbay.db";
-=======
-    protected String URL = "jdbc:sqlite:database/iotbay.db";
->>>>>>> fd18c2fa0c861d1be8e46f6994ab788d1446dc5e:src/main/java/model/dao/DB.java
+    protected String URL = "jdbc:sqlite:D:/Projects/ISD/database/iotbay.db";
     protected String driver = "org.sqlite.JDBC";
     protected Connection conn;
 }

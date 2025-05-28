030209631e9f8f15fcca701e3cff3b223b03d4df
conlanheslop <conlanh2004@gmail.com>
2025-05-28 20:18:57 +1000

    default database url

--- src/main/java/model/dao/DB.java
+++ src/main/java/model/dao/DB.java
@@ -7,7 +7,7 @@ import java.sql.Connection;
  */
 
 public abstract class DB {
-    protected String URL = "jdbc:sqlite:C:/Users/conla/Desktop/.Code/IoTBay/database/iotbay.db";
+    protected String URL = "jdbc:sqlite:iotbay.db"; 
     protected String driver = "org.sqlite.JDBC";
     protected Connection conn;
 }
